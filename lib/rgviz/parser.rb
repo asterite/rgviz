fc43ba701831c8604c1340f604d98abc31e19025
asterite <ary@esperanto.org.ar>
2011-02-07 11:26:07 -0300

    For some reason Time.parse and Date.parse were working

--- lib/rgviz/parser.rb
+++ lib/rgviz/parser.rb
@@ -1,3 +1,6 @@
+require 'time'
+require 'date'
+
 module Rgviz
   class Parser < Lexer
     def initialize(string, options = {})
@@ -5,11 +8,11 @@ module Rgviz
       @query = Query.new
       next_token
     end
-    
+
     def self.parse(string, options = {})
       Parser.new(string, options).parse
     end
-    
+
     def parse
       parse_select
       parse_where
@@ -21,129 +24,129 @@ module Rgviz
       parse_label
       parse_format
       parse_options
-      
+
       raise ParseException.new("Expecting end of query, got: #{@token.string}") if @token.value != Token::EOF
       @query
     end
-    
+
     def parse_select
       return if not token_is! Token::Select
-      
+
       @query.select = Select.new
-      
+
       return if token_is! Token::STAR
-      
+
       parse_columns @query.select.columns
-      
+
       raise ParseException.new("Expecting select columns") if @query.select.columns.empty?
     end
-    
+
     def parse_where
       return if not token_is! Token::Where
-      
+
       @query.where = Where.new parse_expression
     end
-    
+
     def parse_group_by
       return if not token_is! Token::Group
       check! Token::By
-      
+
       @query.group_by = GroupBy.new
       @query.group_by.columns = parse_columns
-      
+
       raise ParseException.new("Expecting group by columns") if @query.group_by.columns.empty?
     end
-    
+
     def parse_pivot
       return if not token_is! Token::Pivot
-      
+
       @query.pivot = Pivot.new
       @query.pivot.columns = parse_columns
-      
+
       raise ParseException.new("Expecting pivot columns") if @query.pivot.columns.empty?
     end
-    
+
     def parse_order_by
       return if not token_is! Token::Order
       check! Token::By
-      
+
       @query.order_by = OrderBy.new
       @query.order_by.sorts = parse_sorts
-      
+
       raise ParseException.new("Expecting order by columns") if @query.order_by.sorts.empty?
     end
-    
+
     def parse_limit
       return if not token_is! Token::Limit
-      
+
       check Token::INTEGER
-      
+
       @query.limit = @token.number
       next_token
     end
-    
+
     def parse_offset
       return if not token_is! Token::Offset
-      
+
       check Token::INTEGER
-      
+
       @query.offset = @token.number
       next_token
     end
-    
+
     def parse_label
       return if not token_is! Token::Label
-      
+
       @query.labels = []
-      
+
       column = parse_column
       raise ParseException.new("Expecting label") unless column
-      
-      check Token::STRING      
-      @query.labels << Label.new(column, @token.string)      
+
+      check Token::STRING
+      @query.labels << Label.new(column, @token.string)
       next_token
-      
+
       while token_is! Token::COMMA
         column = parse_column
         break unless column
-        
-        check Token::STRING      
-        @query.labels << Label.new(column, @token.string)      
+
+        check Token::STRING
+        @query.labels << Label.new(column, @token.string)
         next_token
       end
-      
+
       raise ParseException.new("Expecting label") if @query.labels.empty?
     end
-    
+
     def parse_format
       return if not token_is! Token::Format
-      
+
       @query.formats = []
-      
+
       column = parse_column
       raise ParseException.new("Expecting format") unless column
-      
-      check Token::STRING      
-      @query.formats << Format.new(column, @token.string)      
+
+      check Token::STRING
+      @query.formats << Format.new(column, @token.string)
       next_token
-      
+
       while token_is! Token::COMMA
         column = parse_column
         break unless column
-        
-        check Token::STRING      
-        @query.formats << Format.new(column, @token.string)      
+
+        check Token::STRING
+        @query.formats << Format.new(column, @token.string)
         next_token
       end
-      
+
       raise ParseException.new("Expecting format") if @query.formats.empty?
     end
-    
+
     def parse_options
       return if not token_is! Token::Options
-      
+
       @query.options = Options.new
-      
+
       while true
         case @token.value
         when Token::NoFormat
@@ -156,30 +159,30 @@ module Rgviz
           break
         end
       end
-      
-      raise ParseException.new("Expecting option") if !@query.options.no_format && !@query.options.no_values 
+
+      raise ParseException.new("Expecting option") if !@query.options.no_format && !@query.options.no_values
       raise ParseException.new("Unknown option #{@token.string}") if @token.value != Token::EOF
     end
-    
+
     def parse_sorts
       sorts = []
-    
+
       column = parse_column
       return sorts unless column
-      
+
       order = parse_sort_order
-      
+
       sorts << Sort.new(column, order)
       while token_is! Token::COMMA
         column = parse_column
         break unless column
-        
+
         order = parse_sort_order
         sorts << Sort.new(column, order)
       end
       sorts
     end
-    
+
     def parse_sort_order
       if token_is! Token::Asc
         Sort::Asc
@@ -189,15 +192,15 @@ module Rgviz
         Sort::Asc
       end
     end
-    
+
     def parse_expression
       parse_or_expression
     end
-    
+
     def parse_or_expression
       left = parse_and_expression
       return left unless token_is? Token::Or
-      
+
       operands = [left]
       while true
         if token_is! Token::Or
@@ -207,11 +210,11 @@ module Rgviz
         end
       end
     end
-    
+
     def parse_and_expression
       left = parse_not_expression
       return left unless token_is? Token::And
-      
+
       operands = [left]
       while true
         if token_is! Token::And
@@ -221,7 +224,7 @@ module Rgviz
         end
       end
     end
-    
+
     def parse_not_expression
       if token_is! Token::Not
         operand = parse_primary_expression
@@ -230,7 +233,7 @@ module Rgviz
         parse_primary_expression
       end
     end
-    
+
     def parse_primary_expression
       case @token.value
       when Token::LPAREN
@@ -241,7 +244,7 @@ module Rgviz
       else
         left = parse_column
         raise ParseException.new("Expecting left exp") unless left
-        
+
         case @token.value
         when Token::Is
           next_token
@@ -270,11 +273,11 @@ module Rgviz
         end
       end
     end
-    
+
     def parse_columns(columns = [])
       column = parse_column
       return columns unless column
-      
+
       columns << column
       while token_is! Token::COMMA
         column = parse_column
@@ -283,15 +286,15 @@ module Rgviz
       end
       columns
     end
-    
+
     def parse_column
       parse_arithmetic_expression
     end
-    
+
     def parse_arithmetic_expression
       parse_summation_or_substraction
     end
-    
+
     def parse_summation_or_substraction
       left = parse_multiplication_or_divition
       while true
@@ -309,7 +312,7 @@ module Rgviz
         end
       end
     end
-    
+
     def parse_multiplication_or_divition
       left = parse_atomic_column
       while true
@@ -327,9 +330,9 @@ module Rgviz
         end
       end
     end
-    
+
     def parse_atomic_column
-      case @token.value 
+      case @token.value
       when Token::ID
         return value_column(IdColumn.new(@token.string))
       when Token::Contains, Token::Starts, Token::Ends, Token::With,
@@ -380,7 +383,7 @@ module Rgviz
         else
           return IdColumn.new(string)
         end
-      when Token::Year, Token::Month, Token::Day, 
+      when Token::Year, Token::Month, Token::Day,
            Token::Hour, Token::Minute, Token::Second, Token::Millisecond,
            Token::Now, Token::DateDiff, Token::Lower, Token::Upper,
            Token::Quarter, Token::DayOfWeek, Token::ToDate, Token::Concat
@@ -397,17 +400,17 @@ module Rgviz
       end
       nil
     end
-    
+
     def value_column(col)
       column = col
       next_token
       return column
     end
-    
+
     def token_is?(token_value)
-      @token.value == token_value 
+      @token.value == token_value
     end
-    
+
     def token_is!(token_value)
       if @token.value == token_value
         next_token
@@ -416,11 +419,11 @@ module Rgviz
         false
       end
     end
-    
+
     def check(*token_values)
       raise ParseException.new("Expecting token #{token_values}") unless token_values.any?{|value| @token.value == value}
     end
-    
+
     def check!(*token_values)
       check *token_values
       next_token
